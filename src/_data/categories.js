b63face8b11b99c5bb965d08a0c8c9a81dcebc9b
Chemist Dev <33847652+twochemist@users.noreply.github.com>
2021-04-17 21:54:30 +0200

    Add files via upload

---
+++ src/_data/categories.js
@@ -0,0 +1,50 @@
+module.exports = {
+  tags: [
+    {
+      title: 'Educación',
+      slug: 'education'
+    },
+    {
+      title: 'Aprende a programar',
+      slug: 'learn-to-code'
+    },
+    {
+      title: 'Diversion',
+      slug: 'fun'
+    },
+    {
+      title: 'Ideas',
+      slug: 'ideas'
+    },
+    {
+      title: 'Actividades',
+      slug: 'activities'
+    },
+    {
+      title: 'Apps',
+      slug: 'apps'
+    }
+  ],
+  ages: [
+    {
+      title: 'Front-end',
+      slug: 'front-end'
+    },
+    {
+      title: 'Back-end',
+      slug: 'back-end'
+    },
+    {
+      title: 'Fullstack',
+      slug: 'full-stack'
+    },
+    {
+      title: 'Full Impostor',
+      slug: 'full-impostor'
+    },
+    {
+      title: 'Full Malandriner',
+      slug: 'full-malandriner'
+    }
+  ]
+};
